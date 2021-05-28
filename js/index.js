225bc6f80d1b5cd712b6fe7dc5ade9d3476deb45
Alejandro Gonzalez <alejandro.gonzalez.7e3@itb.cat>
2021-05-29 01:13:15 +0200

    primer commit

---
+++ js/index.js
@@ -0,0 +1,72 @@
+const pacientes = [
+    {
+      paciente: {
+        nombre: "Manuel",
+        edad: 25,
+        sexo: "H",
+      },
+      diasIngresado: 3,
+      dieta: "Baja en fibra",
+    },
+    {
+      paciente: {
+        nombre: "Marta",
+        edad: 56,
+        sexo: "M",
+      },
+      diasIngresado: 5,
+      dieta: "Diabetes",
+    },
+    {
+      paciente: {
+        nombre: "Julia",
+        edad: 38,
+        sexo: "M",
+      },
+      diasIngresado: 1,
+      dieta: "Sin sal",
+    },
+    {
+      paciente: {
+        nombre: "Esteban",
+        edad: 40,
+        sexo: "H",
+      },
+      diasIngresado: 3,
+      dieta: "Diabetes",
+    },
+    {
+      paciente: {
+        nombre: "Arturo",
+        edad: 19,
+        sexo: "H",
+      },
+      diasIngresado: 2,
+      dieta: "Baja en fibra",
+    },
+    {
+      paciente: {
+        nombre: "Isabel",
+        edad: 63,
+        sexo: "M",
+      },
+      diasIngresado: 6,
+      dieta: "Sin sal",
+    },
+  ];
+  
+  
+
+function fltrarPacientes(paciente){
+  const esDiabetico = (paciente) => pacientes.paciente.sexo === "H" && paciente.dieta ==="Diabetes" ;
+}
+
+
+const informeClinico = (pacientes)=>({
+  nHombresDiabeticos = esDiabetico(pacientes)
+});
+
+console.log(informeClinico(pacientes));
+
+
+
